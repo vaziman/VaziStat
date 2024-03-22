687bb1f0c62a5f247e74f2989d5fa2189d2d5e67
mixailkor <60221769+mixailkor@users.noreply.github.com>
2024-03-22 11:36:07 +0100

    Created

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -9,9 +9,13 @@ import android.view.View
 import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.myapplication.constance.Constance
 
 import com.example.myapplication.databinding.ActivityMainBinding
+import com.example.myapplication.fragments.ListMoreFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 
 class MainActivity : AppCompatActivity() {
@@ -22,17 +26,40 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+
         bindingClass = ActivityMainBinding.inflate(layoutInflater)
         setContentView(bindingClass.root)
 
         val toolbar = findViewById<Toolbar>(R.id.toolbarMain)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(toolbar)  // activation return button of toolbar
+        // setSupportActionBar(findViewById(R.id.toolbarMain))
 
-        supportActionBar?.apply {
-            setDisplayHomeAsUpEnabled(true)
-            setHomeAsUpIndicator(R.drawable.arrow_back_for_toolbar)
+        val bottomNavigationMenu = findViewById<BottomNavigationView>(R.id.bottomNavMenu)
+        bottomNavigationMenu.setOnItemSelectedListener { item ->
+            when (item.itemId) {  // set listener on button in bottomMenu
+                R.id.navigation_list -> {
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.CLMain, ListMoreFragment.newInstance()).commit() //open fragment
+
+                    true
+                }
+
+                else -> false
+            }
         }
 
+    }
+
+
 }
-}
+
+
+//             supportActionBar?.apply {
+//           setDisplayHomeAsUpEnabled(true)
+//            setHomeAsUpIndicator(R.drawable.arrow_back_for_toolbar)
+//        }
+
+
 
